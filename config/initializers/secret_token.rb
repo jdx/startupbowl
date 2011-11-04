14b687cdaef8a22ed8414b375e923347720994d8
Jeff Dickey <dickeytk@gmail.com>
2011-11-04 14:16:56 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Startupbowl::Application.config.secret_token = '32dfe3cb4d45e06437c2ff1a428dd5326d9612c0ec847e1cde198068c5cc0a98ea13aaadb7fcb1ea16fe6e8d5f64a79c1ba447c84d0c55fa8730dff5d492650f'
